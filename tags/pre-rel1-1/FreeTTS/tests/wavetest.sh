f08ee26ab1aa18a4bd087d2f5013c1dfa5e42d92
nobody <nobody@4963320b-1a4a-0410-81c8-f0a525965860>
2002-01-18 17:14:16 +0000

    This commit was manufactured by cvs2svn to create tag 'pre-rel1-1'.

---
+++ tags/pre-rel1-1/FreeTTS/tests/wavetest.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# This test script first runs the FreeTTS with our first utterance file,
+# dumps the wave in text form, and compares (diff) it with our standard 
+# wave form file for our first utterance (first.wave.txt). This test will
+# tell you how many lines differ from the standard wave form file.
+#
+# (Reminder: our 'first utterance file' is "Hello world.  This is Duke 
+# coming to you from inside the java virtual machine. I'm happy to have
+# a voice because I've been meaning to tell you how much I care.")
+#
+
+TOP_DIR=..
+PWD=`pwd`
+
+if [ -f wavetest.res ]; then
+	rm wavetest.res
+fi
+
+if [ -f wavetest.diff ]; then
+	rm wavetest.diff
+fi
+
+FREETTS_CLASSES=$TOP_DIR/classes:$TOP_DIR/lib/CommandInterpreter.jar
+if [ -z "${JDK_DIR}" ] ; then
+    JDK_DIR=/lab/speech/java/j2sdk1.4.0
+fi
+
+${JDK_DIR}/bin/java -Xms64m -ea -cp $FREETTS_CLASSES \
+	com.sun.speech.freetts.FreeTTS -silent -dumpWave $PWD/wavetest.res -file $TOP_DIR/wave/08-01-01.wave.text
+
+diff wavetest.res ../data/flite1.1_float.first.wave.txt > wavetest.diff
+
+wc wavetest.diff | awk '
+{
+	if ($1 == 0) {
+	    printf("%s differences in waveTest.res.  Test PASSED\n", $1);
+	} else {
+	    printf("%s differences in waveTest.res.  Test FAILED\n", $1);
+	}
+}
+'
